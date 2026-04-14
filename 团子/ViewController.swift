9c8339bf1223bb312b4c5edee8e017a1f27bc34f
王聪 <p-wangcong40@xiaomi.com>
2026-04-14 16:04:21 +0800

    Initial Commit

---
+++ 团子/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  团子
+//
+//  Created by 王聪 on 2026/4/14.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
